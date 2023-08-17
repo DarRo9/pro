001ea8cba973d67fb6a6204d7c306b728b42361a
DarRo9 <124833469+DarRo9@users.noreply.github.com>
2023-08-17 13:18:49 +0200

    Add files via upload

---
+++ proto.go
@@ -0,0 +1,14 @@
+package proto
+
+import (
+	"golang.org/x/net/context"
+	"log"
+)
+
+type Server struct {
+}
+
+func (s *Server) Do(ctx context.Context, in *Response) (*Response, error) {
+	log.Printf("Receive message body from client: %s", in.Message)
+	return &Response{Message: "Hello From the Server!"}, nil
+}
